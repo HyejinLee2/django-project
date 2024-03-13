17b4175e7c6d2800018a5cfdb148371015f9cf59
Hyejin Lee <149424981+HyejinLee2@users.noreply.github.com>
2024-03-13 06:47:15 +0000

    week10 - theme

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path("about/",views.about, name="about"),
   path('function/', views.function_view, name='function_view'),
   path('class/', views.ClassView.as_view(), name='class_view'),
+  path('theme/', views.ThemeView.as_view(), name='theme'),
   path('hyejin/', views.HyejinView.as_view(), name='Hyejin_view'),
   path('extra/', views.ExtraView.as_view(), name='Extra_view'),
 ]
