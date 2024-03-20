3664367eb5ced8c68555862044183d13646898e0
Hyejin Lee <149424981+HyejinLee2@users.noreply.github.com>
2024-03-20 05:11:46 +0000

    week11_make_invention

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
   path('function/', views.function_view, name='function_view'),
   path('class/', views.ClassView.as_view(), name='class_view'),
   path('theme/', views.ThemeView.as_view(), name='theme'),
+  path('load/', views.load_default_data_view, name='load_default_data'),
+  path('inventions/', views.InventionListView.as_view(), name='invention-list'),
+  path('invention/<int:pk>/', views.InventionDetailView.as_view(), name='invention-view'),
   path('hyejin/', views.HyejinView.as_view(), name='Hyejin_view'),
   path('extra/', views.ExtraView.as_view(), name='Extra_view'),
 ]
